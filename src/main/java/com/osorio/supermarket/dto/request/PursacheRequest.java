03261b6c7dd2ceb7a5630e46a6f653577acba174
Bryan Andrés Osorio Monsalve <bosorio@DUNA.LOCAL>
2023-04-05 11:25:16 -0500

    Avance 05-04-2023

---
+++ src/main/java/com/osorio/supermarket/dto/request/PursacheRequest.java
@@ -0,0 +1,11 @@
+package com.osorio.supermarket.dto.request;
+
+import lombok.Data;
+@Data
+
+public class PursacheRequest {
+    private String namePursache;
+    private String valor;
+    private String color;
+
+}
